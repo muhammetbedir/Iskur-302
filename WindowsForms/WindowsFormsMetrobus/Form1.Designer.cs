e489471fbd47e121d2aadc5d6155711f9f130d68
Muhammet Hüsnü Bedir <bedirmuhammet7@gmail.com>
2022-10-17 11:02:12 +0300

    Duraklar arası gezen metrobüs uygulaması eklendi

---
+++ WindowsForms/WindowsFormsMetrobus/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsMetrobus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(24, 169);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sefere Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1167, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sefere Başla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bahçelievler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(515, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "İncirli";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(731, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Zeytinburnu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1005, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Merter";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-15, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1347, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(-29, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1347, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = resources.GetString("label7.Text");
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1155, 169);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 83);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 372);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsForms/WindowsFormsMetrobus/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsMetrobus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 200;
+            timer1.Start();
+        }
+        int counter = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Left >= 170 && pictureBox1.Left <=185)
+            {
+                Random random = new Random();
+                pictureBox1.Left = pictureBox1.Left;
+                label1.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label1.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox1.Left = pictureBox1.Left + 15;
+
+                }
+            }
+            else if (pictureBox1.Left >= 330 && pictureBox1.Left <= 345)
+            {
+                Random random = new Random();
+                pictureBox1.Left = pictureBox1.Left;
+                label2.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label2.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox1.Left = pictureBox1.Left + 15;
+
+                }
+
+            }
+            else if (pictureBox1.Left >= 510 && pictureBox1.Left <= 525)
+            {
+                Random random = new Random();
+                pictureBox1.Left = pictureBox1.Left;
+                label3.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label3.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox1.Left = pictureBox1.Left + 15;
+
+                }
+
+            }
+            else if (pictureBox1.Left >= 700 && pictureBox1.Left <= 715)
+            {
+                Random random = new Random();
+                pictureBox1.Left = pictureBox1.Left;
+                label4.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label4.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox1.Left = pictureBox1.Left + 15;
+
+                }
+
+            }
+            else if (pictureBox1.Left >= 850 && pictureBox1.Left <= 865)
+            {
+                timer1.Stop();
+
+            }
+            else
+            {
+                pictureBox1.Left = pictureBox1.Left + 15;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+            timer2.Interval = 200;
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            if (pictureBox2.Left >= 170 && pictureBox2.Left <= 185)
+            {
+                Random random = new Random();
+                pictureBox2.Left = pictureBox2.Left;
+                label1.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label1.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox2.Left = pictureBox2.Left - 15;
+
+                }
+            }
+            else if (pictureBox2.Left >= 330 && pictureBox2.Left <= 345)
+            {
+                Random random = new Random();
+                pictureBox2.Left = pictureBox2.Left;
+                label2.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label2.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox2.Left = pictureBox2.Left - 15;
+
+                }
+
+            }
+            else if (pictureBox2.Left >= 510 && pictureBox2.Left <= 525)
+            {
+                Random random = new Random();
+                pictureBox2.Left = pictureBox2.Left;
+                label3.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label3.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox2.Left = pictureBox2.Left - 15;
+
+                }
+
+            }
+            else if (pictureBox2.Left >= 700 && pictureBox2.Left <= 715)
+            {
+                Random random = new Random();
+                pictureBox2.Left = pictureBox2.Left;
+                label4.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                counter++;
+                if (counter == 6)
+                {
+                    label4.BackColor = Color.Transparent;
+                    counter = 0;
+                    pictureBox2.Left = pictureBox2.Left - 15;
+
+                }
+
+            }
+            else if (pictureBox2.Left >= 40 && pictureBox2.Left <= 55)
+            {
+                timer1.Stop();
+
+            }
+            else
+            {
+                pictureBox2.Left = pictureBox2.Left - 15;
+            }
+        }
+    }
+}
